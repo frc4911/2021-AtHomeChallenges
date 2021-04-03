c8aefaa599671314f781a1a3ba9d8c0feafc893a
RedDogFang <kingbros@msn.com>
2021-04-03 10:21:26 -0700

    barrel work

--- src/main/java/frc/robot/auto/modes/BarrelMode.java
+++ src/main/java/frc/robot/auto/modes/BarrelMode.java
@@ -10,8 +10,8 @@ public class BarrelMode extends AutoModeBase {
     @Override
     protected void routine() throws AutoModeEndedException {
         System.out.println("Barrel Auto Mode Running!!!");
-        // runAction(new SetTrajectoryAction(trajectories.barrelPath.get(true), 0.0, 1.0));
-        // runAction(new WaitToFinishPathAction());
+        runAction(new SetTrajectoryAction(trajectories.barrelPath.get(true), 0.0, 1.0));
+        runAction(new WaitToFinishPathAction());
     }
 
 }
